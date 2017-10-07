5b2f4c5d5331055f20ead4a10bd135321bf43abc
Sherwyn Sen <sherwyn.sen@gmail.com>
2017-10-06 17:26:36 -0700

    update assignment tests

--- idea/assignment_test.py
+++ idea/assignment_test.py
@@ -1,9 +1,9 @@
 import json
 
 with open("1244638.json","r") as f:
-		schedule = f.read().strip("[]").split(",")
-		
-for sc in schedule:
-	print(sc)
-	s = json.loads(sc)
+		schedule_s = f.read()
+
+schedule = json.loads(schedule_s)
+
+for s in schedule:
 	print("%s - due: %s, %s" % (s["name"], s["due_at"], ("submitted" if s["has_submitted_submissions"] else "not submitted")))
