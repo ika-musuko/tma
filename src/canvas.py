ec109ab8a07294af976e74d6ed8a29e1bf0d1ef2
Steven V <7stevenvuong@gmail.com>
2017-10-26 01:11:07 -0700

    get_calendar_events, another helper: convert_timestr, reduce redundancy

--- src/canvas.py
+++ src/canvas.py
@@ -15,7 +15,7 @@ canvas_url = "https://sjsu.instructure.com/api/v1%s"
 
 def get_assignments(access_token: str="") -> 'list of Event':
     '''
-    gets the assignments from the courses and creates a list of Events
+    gets the assignments from list of courses and creates a list of Events
     relies on get_courses() and get_headers()
     :param access_token: An access token, or API key, of the Canvas API
     :return: an unsorted list of DueEvents
@@ -27,19 +27,17 @@ def get_assignments(access_token: str="") -> 'list of Event':
     parsed_courses = get_courses(access_token)
     for x in parsed_courses:
         a_course_url = canvas_url % ("/courses/" + str(x['id']) + "/assignments"
-                                                                    + "?include[]=submission"
-                                                                    + "?include[]=assignment_visibility"
-                                                                    + "?include[]=all_dates"
-                                                                    + "?include[]=overrides"
-                                                                    + "?include[]=observed_users")
+                                                                + "?include[]=submission"
+                                                                + "?include[]=assignment_visibility"
+                                                                + "?include[]=all_dates"
+                                                                + "?include[]=overrides"
+                                                                + "?include[]=observed_users"
+                                    )
         parsed_c_asnmt = json.loads(requests.get(a_course_url, headers=get_headers(access_token)).text)
         for y in parsed_c_asnmt:
-            due_at = y['due_at']
-            desc=y['description']
-            if desc is not None:
-                desc=remove_tags(desc)
-            if due_at is not None and y['has_submitted_submissions'] == False:
-                due_at = dateutil.parser.parse(y['due_at'], ignoretz=True)
+            desc = remove_tags(y['description'])
+            due_at = convert_timestr(y['due_at'])
+            if due_at is not None and y['has_submitted_submissions'] is False:
                 if due_at >= today:
                     asnmt.append(event.DueEvent(due=due_at, name=y['name'], desc=desc))
             else:
@@ -48,16 +46,35 @@ def get_assignments(access_token: str="") -> 'list of Event':
     return asnmt
 
 
-def get_courses(access_token: str=""):
+def get_calendar_events(access_token: str="") -> 'list of Event':
+    '''
+    gets a list of calendar events from the calendar_events API endpoint
+    :param access_token: An access token, or API key, of the Canvas API
+    :return: a list of Events
+    '''
+    cal_evs = list()
+    parsed_cal = json.loads(requests.get(canvas_url % "/calendar_events", headers=get_headers(access_token)).text)
+    for x in parsed_cal:
+        desc = remove_tags(x['description'])
+        cal_evs.append(event.Event(name=x['name']
+                                    , desc=desc
+                                    , start=convert_timestr(x['start_at'])
+                                    , end=convert_timestr(x['end_at'])
+                                  )
+                      )
+    return cal_evs
+
+
+def get_courses(access_token: str="")-> 'list of dict':
     '''
     :param access_token: An access token, or API key, of the Canvas API
-    :return: a list of dictionaries, with 'id' and 'name' fields
+    :return: a list of dicts, with 'id' and 'name' fields
     '''
     course_list = list()
     parsed_courses = json.loads(requests.get(canvas_url % "/courses", headers=get_headers(access_token)).text)
     for x in parsed_courses:
         if 'name' in x:
-            crs = {'id' : str(x['id']), 'name' : x['name']}
+            crs = {'id': str(x['id']), 'name': x['name']}
             course_list.append(crs)
     return course_list
 
@@ -69,5 +86,13 @@ def get_headers(access_token: str=""):
     return headers
 
 
-def remove_tags(text):
+def convert_timestr(timestr: None):
+    if timestr is None:
+        return timestr
+    return dateutil.parser.parse(timestr, ignoretz=True)
+
+
+def remove_tags(text: None):
+    if text is None:
+        return text
     return BeautifulSoup(text, "html.parser").get_text()
