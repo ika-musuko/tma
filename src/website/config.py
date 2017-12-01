e746cdc6fb57d0176296f0bef117e84d42217c37
sherwyn <sherwyn.sen@gmail.com>
2017-11-30 18:32:56 -0800

    got rid of swap files...

--- src/website/config.py
+++ src/website/config.py
@@ -11,15 +11,16 @@ if os.environ.get('DATABASE_URL') is None:
     SQLALCHEMY_DATABASE_URI = ('sqlite:///' + os.path.join(basedir, 'app.db') + '?check_same_thread=False')
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+#SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_RECORD_QUERIES = True
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 ### GOOGLE CONFIG ###
 # dynamically load google authorization from file
 with open("gconf.keys", "r") as gconf:
     GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET = gconf.read().splitlines()
     
-print("GOOGLE CRAP %s\n%s" % (GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET))
+#print("GOOGLE CRAP %s\n%s" % (GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET))
 
 ### OAUTH CONFIG ###
 # canvas would be included here as well when that is implemented
